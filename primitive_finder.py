1bdde8452eaa499d190fc1a4d9d17d7f2f284a62
Griffin Scotti <griffinscotti@gmail.com>
2022-10-07 14:15:46 -0700

    finished task 2

--- primitive_finder.py
+++ primitive_finder.py
@@ -1,4 +1,5 @@
 from Crypto.Util.number import GCD
+from Crypto.Util.number import getPrime
 
 #primeRoots function courtesy of MazDak
 #https://stackoverflow.com/questions/40190849/efficient-finding-primitive-roots-modulo-n-using-python
@@ -7,7 +8,8 @@ def primRoots(modulo):
     return [g for g in range(1, modulo) if coprime_set == {pow(g, powers, modulo)
             for powers in range(1, modulo)}]
 
-n = 37
+n = 11
 print(primRoots(n))
+#print(getPrime(2048))
 
 
